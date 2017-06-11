51473a244771abc36908278adf18c10c7968e109
Gregory Langlais <general@gregorylanglais.com>
2017-06-11 15:01:30 -0400

    add timer state management using bitmask

--- src/Timer.js
+++ src/Timer.js
@@ -5,13 +5,44 @@ export default class Timer {
     constructor (config) {
         this.config = new Config(config)
         this.timeout = new Timeout()
+
         this.props = {
             tick: 200,
             time: null, // todo consider config
             elapsed: 0,
             startDate: null,
-            running: false,
+            state: null
         }
+
+        this.state = Timer.states.READY
+    }
+    static get states () {
+        return {
+            READY: 0,
+            STARTED: 1,
+            PAUSED: 2,
+            STOPPED: 4
+        }
+    }
+    set state (state) {
+        switch (state) {
+            case Timer.states.STARTED:
+                this.props.state = Timer.states.READY | Timer.states.STARTED
+                break
+            case Timer.states.PAUSED:
+                this.props.state = Timer.states.READY | Timer.states.STARTED | Timer.states.PAUSED
+                break
+            case Timer.states.STOPPED:
+                this.props.state = Timer.states.READY | Timer.states.STARTED | Timer.states.PAUSED | Timer.states.STOPPED
+                break
+            case Timer.states.READY:
+            default:
+                this.props.state = Timer.states.READY
+                break
+        }
+    }
+    is (state) {
+        return (this.props.state & state) === state
     }
     start () {
         this.timeout.set(() => console.log("done"), this.props.tick)
@@ -20,6 +51,9 @@ export default class Timer {
 
     }
     stop () {
+
+    }
+    reset () {
 
     }
 }
--- test/Timer.js
+++ test/Timer.js
@@ -2,16 +2,16 @@ import Timer from "../"
 import should from "should"
 
 describe('Timer', function() {
-    it('not running prior to calling start', function(done) {
+    it('state is ready with new instance', function(done) {
         let timer = new Timer()
 
         setTimeout(function() {
-            should(timer.props.running).be.false()
-
-            // todo add more prop checks
-
+            should(timer.is(Timer.states.READY)).be.true()
+            should(timer.is(Timer.states.STARTED)).be.false()
+            should(timer.is(Timer.states.PAUSED)).be.false()
+            should(timer.is(Timer.states.STOPPED)).be.false()
             done()
-        }, 1100)
+        }, 1500)
     })
 
     // todo add timer expectations / assertions
