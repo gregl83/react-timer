b5e5194576ca55f3436dd0ceebb55836a36e39a0
Gregory Langlais <general@gregorylanglais.com>
2017-06-23 09:13:10 -0400

    add config events in every scope test

--- test/Config.js
+++ test/Config.js
@@ -186,7 +186,62 @@ describe('Config', () => {
                 done()
             })
 
-            it('multiple phases', done => {
+            it('multiple sets', done => {
+                let config = new Config({
+                    name: 'multi-set',
+                    fixed: false,
+                    sets: [
+                        {
+                            phases: [
+                                {
+                                    name: 'one',
+                                    duration: 20,
+                                    skip: false,
+                                    events: [
+                                        {name: 'alpha', time: 10}
+                                    ]
+                                }
+                            ]
+                        },
+                        {
+                            phases: [
+                                {
+                                    name: 'two',
+                                    duration: 40,
+                                    skip: false,
+                                    events: [
+                                        {name: 'bravo', time: -10}
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                })
+
+                let events = config.events
+
+                should(events[10]).not.be.undefined()
+                should(events[10].constructor.name).be.equal('Array')
+                should(events[10]).be.length(1)
+
+                should(events[10][0].meta.set).be.equal(0)
+                should(events[10][0].meta.phase).be.equal('one')
+                should(events[10][0].event.name).be.equal('alpha')
+                should(events[10][0].event.time).be.equal(10)
+
+                should(events[50]).not.be.undefined()
+                should(events[50].constructor.name).be.equal('Array')
+                should(events[50]).be.length(1)
+
+                should(events[50][0].meta.set).be.equal(1)
+                should(events[50][0].meta.phase).be.equal('two')
+                should(events[50][0].event.name).be.equal('bravo')
+                should(events[50][0].event.time).be.equal(-10)
+
+                done()
+            })
+
+            it('multiple in same phase', done => {
                 let config = new Config({
                     name: 'multi-phase',
                     fixed: false,
@@ -236,147 +291,65 @@ describe('Config', () => {
 
                 done()
             })
-        })
 
-        it('events', done => {
-            let config = new Config({
-                name: 'test',
-                fixed: false,
-                sets: [
-                    {
-                        phases: [
-                            {
-                                name: 'one',
-                                duration: 60,
-                                skip: false,
-                                events: [
-                                    {
-                                        name: 'alpha',
-                                        time: -10
-                                    },
-                                    {
-                                        name: 'bravo',
-                                        time: 10
-                                    }
-                                ]
-                            },
-                            {
-                                name: 'two',
-                                duration: 30,
-                                skip: false,
-                                events: [
-                                    {
-                                        name: 'charlie',
-                                        time: -10
-                                    },
-                                    {
-                                        name: 'delta',
-                                        time: 10
-                                    }
-                                ]
-                            }
-                        ],
-                        events: [
-                            {
-                                name: 'echo',
-                                time: -10
-                            },
-                            {
-                                name: 'foxtrot',
-                                time: 10
-                            }
-                        ]
-                    },
-                    {
-                        phases: [
-                            {
-                                name: 'three',
-                                duration: 120,
-                                skip: true,
-                                events: [
-                                    {
-                                        name: 'gulf',
-                                        time: 10
-                                    },
-                                    {
-                                        name: 'hotel',
-                                        time: -10
-                                    }
-                                ]
-                            },
-                            {
-                                name: 'four',
-                                duration: 60,
-                                skip: true,
-                                events: [
-                                    {
-                                        name: 'india',
-                                        time: 10
-                                    },
-                                    {
-                                        name: 'juliet',
-                                        time: -10
-                                    }
-                                ]
-                            }
-                        ],
-                        events: [
-                            {
-                                name: 'kilo',
-                                time: 10
-                            },
-                            {
-                                name: 'lima',
-                                time: -10
-                            }
-                        ]
-                    }
-                ],
-                events: [
-                    {
-                        name: 'mike',
-                        time: 10
-                    },
-                    {
-                        name: 'november',
-                        time: -10
-                    }
-                ]
+            it('every scope', done => {
+                let config = new Config({
+                    name: 'every-scope',
+                    fixed: false,
+                    sets: [
+                        {
+                            phases: [
+                                {
+                                    name: 'one',
+                                    duration: 60,
+                                    skip: false,
+                                    events: [
+                                        {name: 'alpha', time: 10}
+                                    ]
+                                }
+                            ],
+                            events: [
+                                {name: 'bravo', time: 30}
+                            ]
+                        }
+                    ],
+                    events: [
+                        {name: 'charlie', time: -10}
+                    ]
+                })
+
+                let events = config.events
+
+                should(events[10]).not.be.undefined()
+                should(events[10].constructor.name).be.equal('Array')
+                should(events[10]).be.length(1)
+
+                should(events[10][0].meta.set).be.equal(0)
+                should(events[10][0].meta.phase).be.equal('one')
+                should(events[10][0].event.name).be.equal('alpha')
+                should(events[10][0].event.time).be.equal(10)
+
+                should(events[30]).not.be.undefined()
+                should(events[30].constructor.name).be.equal('Array')
+                should(events[30]).be.length(1)
+
+                should(events[30][0].meta.set).be.equal(0)
+                should(events[30][0].meta.phase).be.null()
+                should(events[30][0].event.name).be.equal('bravo')
+                should(events[30][0].event.time).be.equal(30)
+
+                should(events[50]).not.be.undefined()
+                should(events[50].constructor.name).be.equal('Array')
+                should(events[50]).be.length(1)
+
+                should(events[50][0].meta.set).be.null()
+                should(events[50][0].meta.phase).be.null()
+                should(events[50][0].event.name).be.equal('charlie')
+                should(events[50][0].event.time).be.equal(-10)
+
+                done()
             })
-
-            should(config.events[10].constructor.name).be.equal('Array')
-            should(config.events[10]).be.length(3)
-
-            should(config.events[50].constructor.name).be.equal('Array')
-            should(config.events[50]).be.length(1)
-
-            should(config.events[70].constructor.name).be.equal('Array')
-            should(config.events[70]).be.length(1)
-
-            should(config.events[70].constructor.name).be.equal('Array')
-            should(config.events[70]).be.length(1)
-
-            should(config.events[80].constructor.name).be.equal('Array')
-            should(config.events[80]).be.length(2)
-
-            should(config.events[80].constructor.name).be.equal('Array')
-            should(config.events[80]).be.length(2)
-
-            should(config.events[100].constructor.name).be.equal('Array')
-            should(config.events[100]).be.length(2)
-
-            should(config.events[200].constructor.name).be.equal('Array')
-            should(config.events[200]).be.length(1)
-
-            should(config.events[220].constructor.name).be.equal('Array')
-            should(config.events[220]).be.length(1)
-
-            should(config.events[260].constructor.name).be.equal('Array')
-            should(config.events[260]).be.length(3)
-
-            done()
         })
     })
 
-    // todo - add config tests
 })
